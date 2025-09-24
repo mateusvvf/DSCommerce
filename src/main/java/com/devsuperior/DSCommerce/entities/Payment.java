85e74a342637bcb2c21e1feca7003b53d8c35216
Mateus Viana <mateusvvf@gmail.com>
2025-09-24 17:07:37 -0300

    HashCode and Equals

--- src/main/java/com/devsuperior/DSCommerce/entities/Payment.java
+++ src/main/java/com/devsuperior/DSCommerce/entities/Payment.java
@@ -1,6 +1,7 @@
 package com.devsuperior.DSCommerce.entities;
 
 import java.time.Instant;
+import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -61,6 +62,22 @@ public class Payment {
 	public void setOrder(Order order) {
 		this.order = order;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		return Objects.equals(id, other.id);
+	}
 	
 }
