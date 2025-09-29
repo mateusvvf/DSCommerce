03578fc9ecd67784eb66a7edde21b6993f4b7658
Mateus Viana <mateusvvf@gmail.com>
2025-09-29 15:09:53 -0300

    GET Product by id

---
+++ src/main/java/com/devsuperior/DSCommerce/controllers/ProductController.java
@@ -0,0 +1,28 @@
+package com.devsuperior.DSCommerce.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.DSCommerce.dto.ProductDTO;
+import com.devsuperior.DSCommerce.services.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+	
+	@Autowired
+	private ProductService service;
+	
+	
+	@GetMapping(value = "/{id}")
+	public ProductDTO findById(@PathVariable Long id) {
+		ProductDTO dto = service.findById(id);
+		return dto;
+	}
+	
+}
