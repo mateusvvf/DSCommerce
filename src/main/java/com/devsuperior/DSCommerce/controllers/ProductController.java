f8facf49268877e5b7aa252aef307664210a0a20
Mateus Viana <mateusvvf@gmail.com>
2025-09-29 15:49:37 -0300

    GET Paginated products

--- src/main/java/com/devsuperior/DSCommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/DSCommerce/controllers/ProductController.java
@@ -2,6 +2,8 @@ package com.devsuperior.DSCommerce.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,12 @@ public class ProductController {
 	
 	@Autowired
 	private ProductService service;
+	
+	
+	@GetMapping
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		return service.findAll(pageable);
+	}
 	
 	
 	@GetMapping(value = "/{id}")
